47898e4d89cb813e918acbbc04fd94663084db4c
Nikita-mnd <nikita.morozov.mnd@gmail.com>
2025-03-15 23:05:59 +0300

    logic

--- task01/logic.cpp
+++ task01/logic.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int min(int* m, int size) {
 	int min = 0;
 	for (int i = 0;i <= size;i++) {
-		if (m[min] > m[i]) {
+		int num = m[i];
+		num *= m[i] < 0 ? -1 : 1;
+		if (m[min] > num) {
 			min = i;
 		}
 	}
@@ -14,10 +16,27 @@ int min(int* m, int size) {
 int max(int* m, int size) {
 	int max = 0;
 	for (int i = 0;i <= size;i++) {
-		if (m[max] > m[i]) {
+		int num = m[i];
+		num *= m[i] < 0 ? -1 : 1;
+		if (m[max] > num) {
 			max = i;
 		}
 	}
 	return max;
 }
 
+int sum(int* m, int size) {
+	if (size <= 0) {
+		return 0;
+	}
+	int start = min(m, size);
+	int end = max(m, size);
+	if (start > end) {
+		swap(start, end);
+	}
+	int sum = 0;
+	for (int i = start;i < end;i++) {
+		sum += m[i];
+	}
+	return sum;
+}
--- task01/main.cpp
+++ task01/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
-
+int sum(int* m, int size);
 int main(){
+	cout << "input size of masive: ";
+	int size;
+	cin >> size;
+	int* m = new int[size];
 
-
+	for (int i = 0;i <= size;i++) {
+		m[i] = rand() / 10;
+	}
+	int result = sum(m, size);
+	cout << "sum of number: " << result;
+	delete[] m;
 	return 0;
 }
